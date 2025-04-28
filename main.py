f437b6b8a8b6ffe91dbc018a4551ecc8c27c9cd9
sanchxtene <84470734+sanchxtene@users.noreply.github.com>
2025-04-28 17:56:09 -0300

    Unindo os branches

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
 import time
 import math
+import sys
 
 # função para ler o arquivo de dados
-def ler_arquivo_equipamentos(nome_arquivo):
+def ler_arquivo_equipamentos():
 
-    with open(nome_arquivo, 'r') as arquivo:
+    # sys.argv[1]: caminho do arquivo de entrada
+    with open(sys.argv[1], 'r') as arquivo:
         linha1 = arquivo.readline().strip().split()
         orcamento = float(linha1[0])
         n = int(linha1[1])
@@ -75,7 +77,7 @@ def atualiza_poder_sinergias(poder_atual, equipamentos, sinergias, indices, remo
 
 
 
-# busca local que escolhe o vizinho usando fist improvement
+# busca local que escolhe o vizinho usando first improvement
 def busca_local(equipamentos, sinergias, orcamento, solucao_inicial, indices_iniciais):
     otimo_local = solucao_inicial.copy()
     indices_otimo_local = indices_iniciais.copy()
@@ -84,6 +86,7 @@ def busca_local(equipamentos, sinergias, orcamento, solucao_inicial, indices_ini
     indices_para_testar = indices_otimo_local.copy()
     vizinhos = list(set(range(len(equipamentos))) - set(indices_otimo_local))
 
+    tempo_inicio = time.time()
     while melhorou:
         melhorou = False
         random.shuffle(vizinhos)
@@ -112,14 +115,19 @@ def busca_local(equipamentos, sinergias, orcamento, solucao_inicial, indices_ini
                     otimo_local = [equipamentos[x] for x in novos_indices]
                     indices_otimo_local = novos_indices
                     poder_otimo_local = novo_poder
+                    # tempo decorrido
+                    print(f"Tempo de troca: {time.time() - tempo_inicio:.4f} segundos")
+                    # novo melhor local
+                    print (poder_otimo_local)
+                    # visualização_solução()
+                        #precisa de uma função, fazer depois que resolver o resto
                     indices_para_testar = indices_otimo_local.copy()
                     melhorou = True
                     vizinhos = list(set(range(len(equipamentos))) - set(indices_otimo_local))
                     break
 
-
-
-    return otimo_local, indices_otimo_local,vizinhos
+    tempo_final = time.time()
+    return otimo_local, indices_otimo_local,vizinhos, tempo_inicio, tempo_final
 
 
 # perturbacao nos indices atuais da busca local
@@ -148,7 +156,11 @@ def perturbacao(indices_atuais, vizinhos, fator):
 
 
 if __name__ == '__main__':
-    orcamento, equipamentos, sinergias = ler_arquivo_equipamentos('dados.txt')
+    # receber por linha de comando:
+    # sys.argv[1]: caminho do arquivo de entrada
+    # sys.argv[2]: valor de critério de parada
+    # sys.argv[3]: parametro de variação
+    orcamento, equipamentos, sinergias = ler_arquivo_equipamentos()
 
     ia = time.time()
     sol_inicial, indices_inicial = solucao_inicial(orcamento, equipamentos, sinergias)
@@ -157,9 +169,7 @@ if __name__ == '__main__':
     print("Poder inicial:", soma_poder_sinergias(sol_inicial, sinergias, indices_inicial))
     print(f"Tempo de execução: {fa - ia:.4f} segundos")
 
-    ib = time.time()
-    sol_final, indices_final,vizinhos = busca_local(equipamentos, sinergias, orcamento, sol_inicial, indices_inicial)
-    fb = time.time()
+    sol_final, indices_final, vizinhos, ib, fb = busca_local(equipamentos, sinergias, orcamento, sol_inicial, indices_inicial)
     print("Melhor solução:",  indices_final)
     print("Melhor poder:", soma_poder_sinergias(sol_final, sinergias, indices_final))
     print(f"Tempo de execução: {fb - ib:.4f} segundos")
